5cf2c70b056b88d6c09574f626e627aec280c063
MartaKoss27 <27691497m@gmail.com>
2023-11-06 19:27:47 +0100

    init 2

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,78 +1,107 @@
-//import React, { Component } from 'react';
-import { useEffect, useState } from 'react';
+import React, { Component } from 'react';
 import { Form } from './Form';
 import { Contacts } from './Contacts';
 import { Filter } from './Filter';
-//import { nanoid } from 'nanoid';
+import { nanoid } from 'nanoid';
 import Notiflix from 'notiflix';
 import { styled } from 'styled-components';
 
-export const App = () => {
-  const [contacts, setContacts] = useState(
-    () => JSON.parse(localStorage.getItem('contacts')) ?? []
-  );
-  const [filter, setFilter] = useState('');
+export class App extends Component {
+  state = {
+    contacts: [],
+    filter: '',
+    name: '',
+    number: '',
+  };
+
+  //nanoid = nanoid();
+
+  //FORM
+  handleSubmit = event => {
+    event.preventDefault();
+    const form = event.currentTarget;
+
+    const {
+      name: { value: name },
+      number: { value: number },
+    } = form.elements;
+
+    const contactExists = this.state.contacts.find(
+      contact => contact.name.toLowerCase() === name.toLowerCase()
+    );
+
+    if (contactExists) {
+      Notiflix.Notify.warning(`${name} is already in contacts.`);
+    } else {
+      const newContact = {
+        id: nanoid(),
+        name,
+        number,
+      };
+
+      this.setState(prevState => ({
+        contacts: [...prevState.contacts, newContact],
+      }));
+      Notiflix.Notify.success('New contact succesfully added!');
+      form.reset();
+    }
+  };
+
+  //CONTACTS
+  handleClick = id => {
+    const { contacts } = this.state;
+    const contactToRemove = contacts.find(contact => contact.id === id);
+
+    if (contactToRemove) {
+      this.setState(prev => ({
+        contacts: prev.contacts.filter(contact => contact.id !== id),
+      }));
+
+      Notiflix.Notify.success(`${contactToRemove.name} has been removed`);
+    }
+  };
+  //FILTER
+  handleFilterChange = e => {
+    this.setState({ filter: e.currentTarget.value.toLowerCase() });
+  };
 
   //LOCAL STORAGE
 
-  useEffect(() => {
-    const savedContacts = localStorage.getItem('contacts');
-    if (savedContacts) {
-      setContacts(JSON.parse(savedContacts));
+  componentDidMount() {
+    const data = localStorage.getItem('contacts');
+    const parsedData = JSON.parse(data);
+
+    if (parsedData) {
+      return this.setState({ contacts: [...parsedData] });
     }
-  }, []);
+  }
 
-  useEffect(() => {
-    localStorage.setItem('contacts', JSON.stringify(contacts));
-  }, [contacts]);
-
-  const handleAddContact = newContact => {
-    const existingContact = contacts.find(
-      contact => contact.name.toLowerCase() === newContact.name.toLowerCase()
-    );
-
-    if (existingContact) {
-      Notiflix.Notify.warning(`${newContact.name} is already in contacts.`);
-      return;
+  componentDidUpdate(prevProps, prevState) {
+    const { contacts } = this.state;
+    if (prevState.contacts !== contacts) {
+      const data = JSON.stringify(contacts);
+      localStorage.setItem('contacts', data);
     }
+  }
 
-    setContacts(prevContacts => [...prevContacts, newContact]);
-  };
-
-  const handleDeleteContact = contactId => {
-    setContacts(prevContacts =>
-      prevContacts.filter(contact => contact.id !== contactId)
+  render() {
+    const { contacts, filter } = this.state;
+    const filterSearch = contacts.filter(contact =>
+      contact.name.toLowerCase().includes(this.state.filter)
     );
-  };
-
-  const handleFilterChange = event => {
-    setFilter(event.target.value);
-  };
-
-  const getFilteredContacts = () => {
-    return contacts.filter(contact =>
-      contact.name.toLowerCase().includes(filter.toLowerCase())
+    return (
+      <StyledWrapper>
+        <StyledDiv>
+          <h1>Phonebook</h1>
+          <Form onSubmit={this.handleSubmit} />
+          <h2>Contacts list</h2>
+          <Filter onChange={this.handleFilterChange} filter={filter} />
+          <Contacts contacts={filterSearch} removeContact={this.handleClick} />
+        </StyledDiv>
+      </StyledWrapper>
     );
-  };
-
-  const filteredContacts = getFilteredContacts();
-
-  return (
-    <StyledWrapper>
-      <StyledDiv>
-        <h1>Phonebook</h1>
-        <Form onSubmit={handleAddContact} />
-        <h2>Contacts list</h2>
-        <Filter onChange={handleFilterChange} filter={filter} />
-        <Contacts
-          contacts={filteredContacts}
-          removeContact={handleDeleteContact}
-        />
-      </StyledDiv>
-    </StyledWrapper>
-  );
-};
-
+  }
+}
 const StyledWrapper = styled.div`
   height: 100vh;
   font-size: 30px;
